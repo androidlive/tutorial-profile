3b2d021f7c25c404824b91b4ee488cc8c7a5f177
marko <marko.zivojnovic@pureagency.com>
2018-11-22 22:41:53 +0100

    Profile Tutorial
    androidlive

---
+++ app/src/main/java/com/tutorial/app/profile/appprofiletutorial/ProfileListActivity.java
@@ -0,0 +1,14 @@
+package com.tutorial.app.profile.appprofiletutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_list);
+    }
+}
